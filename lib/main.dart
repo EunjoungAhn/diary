6ed3ec485e1707e50a86b7f165da6a401419a103
EunjoungAhn <aej9300@gmail.com>
2022-12-08 19:57:14 +0900

    함수를 통해서 setState를 할 수 있지만 바로, initState에서는 setState를 할 수 없어서 함수화로 만든 후, initState에 넣어주는 것이다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:diary/data/database.dart';
 import 'package:diary/data/diary.dart';
 import 'package:diary/data/util.dart';
 import 'package:diary/write.dart';
@@ -35,6 +36,28 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   int selectIndex = 0;
+
+  // DB에 저장된 값 가져오기 위해 설정
+  final dbHelper = DatabaseHelper.instance;
+  Diary todatDiary;
+
+  void getTodayDiary() async {
+    List<Diary> diary = await dbHelper.getDiaryByDate(Utils.getFormatTime(DateTime.now()));
+    if(diary.isEmpty){
+      todatDiary = diary.first;
+    }
+    // 함수를 통해서 setState를 할 수 있지만 바로, initState에서는 setState를 할 수 없어서
+    // 함수화로 만든 후, initState에 넣어주는 것이다.
+    setState(() {
+
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTodayDiary();
+  }
 
   @override
   Widget build(BuildContext context) {
