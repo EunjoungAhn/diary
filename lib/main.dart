8fb6d5020f69816a2792a1b2b8e650ac476e013f
EunjoungAhn <aej9300@gmail.com>
2022-12-08 20:35:13 +0900

    저장시 데이텨 표현되는 함수 오타 수정

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,20 @@ class _MyHomePageState extends State<MyHomePage> {
 
   int selectIndex = 0;
 
+  List<String> statusimg = [
+    "assets/img/ico-weather.png",
+    "assets/img/ico-weather_2.png",
+    "assets/img/ico-weather_3.png",
+  ];
+
   // DB에 저장된 값 가져오기 위해 설정
   final dbHelper = DatabaseHelper.instance;
-  Diary todatDiary;
+  Diary todayDiary;
 
   void getTodayDiary() async {
     List<Diary> diary = await dbHelper.getDiaryByDate(Utils.getFormatTime(DateTime.now()));
-    if(diary.isEmpty){
-      todatDiary = diary.first;
+    if(diary.isNotEmpty){
+      todayDiary = diary.first;
     }
     // 함수를 통해서 setState를 할 수 있지만 바로, initState에서는 setState를 할 수 없어서
     // 함수화로 만든 후, initState에 넣어주는 것이다.
@@ -67,7 +73,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Container(
         child: getPage(),
-
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -80,6 +85,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 image: "assets/img/b1.jpg"
               ),
             )));
+           getTodayDiary();
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
@@ -119,7 +125,37 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Widget getTodayPage(){
-    return Container();
+    if(todayDiary == null){
+      return Container(
+        child: Text("일기를 작성해 주세요."),
+      );
+    }
+    return Container(
+      child: Stack(
+        children: [
+          // 배경 이미지를 깔리위한 설정
+          Positioned.fill(
+              child: Image.asset(todayDiary.image, fit: BoxFit.cover,),
+          ),
+          // 배경위에 데이터를 표시하기 위해 설정
+          Positioned.fill(
+              child: ListView(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("${DateTime.now().month}.${DateTime.now().day}",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,
+                      color: Colors.white),),
+                      Image.asset(statusimg[todayDiary.status], fit: BoxFit.contain,)
+                    ],
+                  )
+                ],
+              )
+          ),
+        ],
+      ),
+    );
   }
 
   Widget getHistoryPage(){
